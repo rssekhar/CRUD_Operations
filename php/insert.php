c89f5dd2045890342c61564c17aa9e6a46ae572a
rssekhar <93420682+rssekhar@users.noreply.github.com>
2022-03-10 13:36:51 +0530

    Update insert.php

--- php/insert.php
+++ php/insert.php
@@ -1,12 +1,21 @@
+
 <?php
 include("connect.php");
+
 
 $data = stripslashes(file_get_contents("php://input"));
 $mydata = json_decode($data,true);
 $id = $mydata['id'];//defined
-$name = $mydata['name'];
+$fname = $mydata['fname'];
+$lname = $mydata['lname'];
+
 $email = $mydata['email'];
+$dob = $mydata['dob'];
 $number = $mydata['number'];
+$country = $mydata['country'];
+$bio = $mydata['bio'];
+
+// $fnameErr = $lnameErr = $emailErr = $dobErr = $numberErr = $countryErr = $bioErr = "";
 
 //insert data
 // if(!empty($name) && !empty($email) && !empty($number)){
@@ -23,8 +32,15 @@ $number = $mydata['number'];
 // }
 
 //add or update user data
-if(!empty($name) && !empty($email) && !empty($number)){
-    $sql = "INSERT INTO registerdata(id,name,email,number) VALUES ('$id','$name','$email','$number') ON DUPLICATE KEY UPDATE name='$name',email='$email',number='$number'";
+
+// $error = "";
+
+    
+if(!empty($fname) && !empty($lname)&&!empty($email) && !empty($dob) && !empty($number) && !empty($country) && !empty($bio)){
+  
+
+   $sql = "INSERT INTO userinfo(id,fname,lname,email,dob,number,country,bio) VALUES ('$id','$fname','$lname','$email','$dob','$number','$country','$bio') ON DUPLICATE KEY UPDATE 
+            fname='$fname',lname='$lname',email='$email',dob='$dob',number='$number',country='$country',bio='$bio'";
     if($conn->query($sql) == TRUE){
         echo "User Added";
     }
@@ -33,7 +49,91 @@ if(!empty($name) && !empty($email) && !empty($number)){
     }
 }
 else{
-    echo "Please Fill All Fields";
-}
+  $fnameErr = $lnameErr =$emailErr = $dobErr = $numberErr = $countryErr = $bioErr = "";
 
+  // fname validation
+    if(empty($fname)){
+        $fnameErr = "<br>first name is required";
+        echo $fnameErr;
+    }
+    else{
+          
+      if (!preg_match("/^[a-zA-Z-' ]*$/",$fname)) {
+        $fnameErr = "<br>first name has Only letters and white space are allowed";
+        echo $fnameErr;
+      }
+    }
+    // lname validation
+    if(empty($lname)){
+        // alert("last name is required");
+        $lnameErr = "<br>last name is required";
+        echo $lnameErr;
+    }
+    else{
+          
+      if (!preg_match("/^[a-zA-Z-' ]*$/",$lname)) {
+        $lnameErr = "<br>last name has Only letters and white space are allowed";
+        echo $lnameErr;
+      }
+    }
+    
+    // email validation
+    if(empty($email)){
+        // alert("email is required");
+        $emailErr = "<br>email is required";
+        echo $emailErr;
+    }
+    else{
+      $pattern = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^";   
+      if (!preg_match($pattern,$email)) {
+        $emailErr = "<br>Wrong Email Format";
+        echo $emailErr;
+      }
+    }
+    // dob validation
+    if(empty($dob)){
+        $dobErr = "<br>date of birth is required";
+        echo $dobErr;
+    }
+    if(empty($number)){
+
+        $numberErr = "<br>number is required";
+        echo $numberErr;
+    }
+    // number validation
+    else{
+      if (!preg_match("/^[7-9][0-9]{0,10}$/",$number)) {
+        $numberErr = "<br>Mobile no must contain 10 digits only.";
+        echo $numberErr;
+      }
+       
+    }
+    // country validation
+    if(empty($country)){
+        $countryErr = "<br>country is required";
+        echo $countryErr;
+    }
+    else{
+          
+      if (!preg_match("/^[a-zA-Z-' ]*$/",$country)) {
+        $countryErr = "<br>Only letters and white space allowed";
+        echo $countryErr;
+      }
+    }
+    // bio validation
+    if(empty($bio)){
+        
+        $bioErr = "<br>bio is required";
+        echo $bioErr;
+    }
+    else{
+      $length = strlen($bio);    
+      
+      if($length != 150){
+        $bioErr = "<br>bio must be 150 characters only";
+        echo $bioErr;
+      }
+      
+    }
+}
 ?>
